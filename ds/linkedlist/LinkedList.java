67daa06af30490cff200b1c7981d660852b79d52
am22ar <amarpatil12222@gmail.com>
2022-10-08 19:34:25 +0530

    Added: Create Linked List logic

--- ds/linkedlist/LinkedList.java
+++ ds/linkedlist/LinkedList.java
@@ -1,8 +1,27 @@
 package com.ds.linkedlist;
 
 public class LinkedList {
+	Node head;
+	class Node
+	{
+		Object data;
+		Node ref;
+		Node(Object data)
+		{
+			this.data = data;
+		}
+	}
+	public void elements(Object data)
+	{
+		Node newNode = new Node(data);
+		System.out.print(newNode.data+" ");
+	}
 	public static void main(String[] args) {
-		System.out.println("Welcome to Linked List Program");
+		LinkedList list = new LinkedList();
+		System.out.println("Welcome to LinkedList program");
+		list.elements(56);
+		list.elements(30);
+		list.elements(70);
 	}
 	
 }
